ff98d14ee156a7764afcf73c6660bcc2a45b78d8
DevStranger <143540628+DevStranger@users.noreply.github.com>
2025-06-06 10:29:06 +0200

    Update creature.h

--- client/src/creature.h
+++ client/src/creature.h
@@ -1,6 +1,8 @@
 #ifndef CREATURE_H
 #define CREATURE_H
 
+#include <time.h>
+
 typedef struct {
     int hunger;     // 0-glodny, 100-najedzony
     int happiness;  // 0-smutny, 100-szczesliwy
@@ -9,9 +11,15 @@ typedef struct {
     int growth;     // 0-brak rozwoju, 100-rozwiniety
     int love;       // 0-samotny, 100-kochany i kochajacy
     int level;      // ogolny poziom gry
+
+    time_t last_update1;
+    time_t last_update2;
+    time_t last_update3;
+    time_t last_update4;
+
 } Creature;
 
 void init_creature(Creature *c);
-void update_creature(Creature *c, float delta_seconds);
+void update_creature(Creature *c);
 
 #endif
