bbaa5c0b8737ea610d570fc0f2f8f928c4c3e7c2
DevStranger <143540628+DevStranger@users.noreply.github.com>
2025-06-06 10:02:22 +0200

    Update protocol.h

--- common/protocol.h
+++ common/protocol.h
@@ -6,4 +6,20 @@
 int send_msg(int sockfd, const char *json_str);
 int recv_msg(int sockfd, char *buffer, int bufsize);
 
+typedef enum {
+    MSG_CONNECT,
+    MSG_PAIR,
+    MSG_ACTION,
+    MSG_RESULT,
+    MSG_DISCONNECT,
+} MessageType;
+
+typedef struct {
+    MessageType type;
+    int session_id;      // ID sesji gracza
+    int player_id;       // np. 0 lub 1
+    int action_code;     // kod akcji (np. kliknięcie guzika)
+    char payload[256];   // dodatkowe dane
+} Message;
+
 #endif
