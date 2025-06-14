9fab60dfa15e728f3cab8573e0e52675fce7203c
DevStranger <143540628+DevStranger@users.noreply.github.com>
2025-06-14 19:57:51 +0200

    Update server.c

--- server.c
+++ server.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #define PORT 12345
 #define MAX_CLIENTS 10
@@ -14,96 +15,121 @@ typedef struct {
     char char2;
     int has_char1;
     int has_char2;
+    pthread_mutex_t lock;
 } Pair;
 
 Pair pairs[MAX_CLIENTS / 2];
 int pair_count = 0;
+pthread_mutex_t pair_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void handle_client(int client_sock) {
-    // Pairing logic
+void *handle_client(void *arg) {
+    int client_sock = *(int *)arg;
+    free(arg);
     Pair *assigned_pair = NULL;
+    int is_first;
 
+    // Szukamy pary lub tworzymy nową
+    pthread_mutex_lock(&pair_mutex);
     for (int i = 0; i < pair_count; ++i) {
         if (pairs[i].client2 == -1) {
             pairs[i].client2 = client_sock;
             assigned_pair = &pairs[i];
+            is_first = 0;
             break;
         }
     }
-
     if (!assigned_pair) {
         if (pair_count >= MAX_CLIENTS / 2) {
             printf("Too many clients!\n");
             close(client_sock);
-            return;
+            pthread_mutex_unlock(&pair_mutex);
+            return NULL;
         }
-        pairs[pair_count].client1 = client_sock;
-        pairs[pair_count].client2 = -1;
-        pairs[pair_count].has_char1 = 0;
-        pairs[pair_count].has_char2 = 0;
         assigned_pair = &pairs[pair_count];
+        assigned_pair->client1 = client_sock;
+        assigned_pair->client2 = -1;
+        assigned_pair->has_char1 = 0;
+        assigned_pair->has_char2 = 0;
+        pthread_mutex_init(&assigned_pair->lock, NULL);
+        is_first = 1;
         pair_count++;
     }
+    pthread_mutex_unlock(&pair_mutex);
 
+    // Odbierz znak
     char buffer;
     if (recv(client_sock, &buffer, 1, 0) <= 0) {
         perror("recv");
         close(client_sock);
-        return;
+        return NULL;
     }
 
-    // Store the character
-    if (assigned_pair->client1 == client_sock) {
+    // Zapisz znak w parze
+    pthread_mutex_lock(&assigned_pair->lock);
+    if (is_first) {
         assigned_pair->char1 = buffer;
         assigned_pair->has_char1 = 1;
-    } else if (assigned_pair->client2 == client_sock) {
+    } else {
         assigned_pair->char2 = buffer;
         assigned_pair->has_char2 = 1;
     }
+    pthread_mutex_unlock(&assigned_pair->lock);
 
-    // Wait for the other client's input
-    while (!(assigned_pair->has_char1 && assigned_pair->has_char2)) {
-        usleep(1000);
+    // Czekaj aż obaj wyślą znaki
+    while (1) {
+        pthread_mutex_lock(&assigned_pair->lock);
+        if (assigned_pair->has_char1 && assigned_pair->has_char2) {
+            char to_send = is_first ? assigned_pair->char2 : assigned_pair->char1;
+            pthread_mutex_unlock(&assigned_pair->lock);
+            send(client_sock, &to_send, 1, 0);
+            break;
+        }
+        pthread_mutex_unlock(&assigned_pair->lock);
+        usleep(10000); // odciążenie CPU
     }
 
-    // Send back the partner's character
-    char partner_char = (assigned_pair->client1 == client_sock) ? assigned_pair->char2 : assigned_pair->char1;
-    send(client_sock, &partner_char, 1, 0);
-
     close(client_sock);
+    return NULL;
 }
 
 int main() {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == 0) {
-        perror("socket failed");
+    if (server_fd == -1) {
+        perror("socket");
         exit(EXIT_FAILURE);
     }
 
     struct sockaddr_in address = {
         .sin_family = AF_INET,
-        .sin_addr.s_addr = INADDR_ANY,
-        .sin_port = htons(PORT)
+        .sin_port = htons(PORT),
+        .sin_addr.s_addr = INADDR_ANY
     };
 
-    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-    listen(server_fd, MAX_CLIENTS);
-    printf("Server is listening on port %d...\n", PORT);
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, MAX_CLIENTS) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Serwer nasłuchuje na porcie %d\n", PORT);
 
     while (1) {
-        int addrlen = sizeof(address);
-        int client_sock = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-        if (client_sock < 0) {
+        struct sockaddr_in client_addr;
+        socklen_t addrlen = sizeof(client_addr);
+        int *client_sock = malloc(sizeof(int));
+        *client_sock = accept(server_fd, (struct sockaddr *)&client_addr, &addrlen);
+        if (*client_sock < 0) {
             perror("accept");
             continue;
         }
-        if (fork() == 0) {
-            // Child process
-            close(server_fd);
-            handle_client(client_sock);
-            exit(0);
-        }
-        close(client_sock); // Parent doesn't need this
+
+        pthread_t tid;
+        pthread_create(&tid, NULL, handle_client, client_sock);
+        pthread_detach(tid);
     }
 
     return 0;
