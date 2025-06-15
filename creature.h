95e113ab2ce08e8930c5903efb69d3b6519ff8fc
DevStranger <143540628+DevStranger@users.noreply.github.com>
2025-06-15 11:41:03 +0200

    Create creature.h

---
+++ creature.h
@@ -0,0 +1,24 @@
+#ifndef CREATURE_H
+#define CREATURE_H
+
+#include <time.h>
+
+typedef struct {
+    int hunger;
+    int happiness;
+    int sleep;
+    int health;
+    int growth;
+    int love;
+    int level;
+
+    time_t last_update1;
+    time_t last_update2;
+    time_t last_update3;
+    time_t last_update4;
+} Creature;
+
+void init_creature(Creature *c);
+void update_creature(Creature *c);
+
+#endif // CREATURE_H
