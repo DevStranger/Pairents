afb252a7313100b94eb3b17630b037470f87effa
DevStranger <143540628+DevStranger@users.noreply.github.com>
2025-06-15 10:19:44 +0200

    Update GUI.h

--- GUI.h
+++ GUI.h
@@ -7,28 +7,19 @@
 
 #define BUTTON_COUNT 5
 
-// Inicjalizacja SDL i czcionek
-int GUI_Init(void);
+// Inicjalizacja zasobów GUI (czcionki, ASCII art) na podstawie podanego renderer'a SDL
+// Zwraca 1 w przypadku sukcesu, 0 w przypadku błędu
+int GUI_init(SDL_Renderer *renderer);
 
-// Renderer
+// Zwalnia zasoby używane przez GUI (czcionki, ASCII art)
+void GUI_cleanup(void);
+
+// Renderuje GUI: statystyki stworzenia, ASCII art oraz przyciski
 void GUI_render(SDL_Renderer *renderer, const creature_t *creature);
 
-// Tworzy okno z tytułem
-int GUI_CreateWindow(const char *title);
-
-// Rysuje przyciski
-void GUI_DrawButtons(SDL_Rect buttons[]);
-
-// Inicjalizuje pozycje przycisków
-void GUI_InitButtons(SDL_Rect buttons[]);
-
-// Obsługuje kliknięcia, zwraca indeks klikniętego przycisku lub -1
-int GUI_HandleClick(int x, int y, SDL_Rect buttons[]);
-
-// Zamyka okno i renderer
-void GUI_Destroy(void);
-
-// Czyści SDL i TTF
-void GUI_Quit(void);
+// Sprawdza, czy kliknięcie (x, y) mieści się w którymś z przycisków
+// Zwraca indeks przycisku lub -1, jeśli poza przyciskami
+int GUI_check_button_click(int x, int y);
 
 #endif // GUI_H
+
