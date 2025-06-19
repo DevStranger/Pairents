a946605680a3dd54dd6fffc8f7bef7aeab88604f
DevStranger <143540628+DevStranger@users.noreply.github.com>
2025-06-19 08:04:41 +0200

    Update client.c

--- client.c
+++ client.c
@@ -207,7 +207,7 @@ int main(int argc, char *argv[]) {
                         // Rozpocznij odbiór nieblokujący stwora
                         reset_creature_receiver();
                         waiting_for_creature = 1;
-                        waiting_for_Response = 0;
+                        waiting_for_response = 0;
                         break;
                     case 2:
                         printf("Wynik: oczekiwanie na drugiego gracza.\n");
